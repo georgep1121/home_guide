38a73863587bde6a8695174ca8668d4ffcda79d3
georgep1121 <g.papastavrou@outlook.com>
2026-06-14 20:11:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ let scrollPosition = 0;
 
 /* DATA */
 const posts = [
-  { title: "Main Entrance", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Main entrance overview and layout." },
-  { title: "Front Yard", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Front yard design and structure." },
-  { title: "Rear Yard", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rear yard planning and usage." },
-  { title: "Rear Patio", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rear patio functional space design." },
-  { title: "Rear Lower Entrance", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lower rear access and circulation." },
-  { title: "Lower Living Room", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lower level living room arrangement." },
-  { title: "Internal Stairs", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Staircase flow and vertical connection." },
-  { title: "Upper Hall", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Upper hall circulation space." },
-  { title: "Upper Master Bedroom", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Master bedroom layout and comfort." },
-  { title: "Upper Living Room", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Upper living area design concept." },
-  { title: "Upper Front Entrance", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Upper front access point design." },
-  { title: "Sewage & Wastewater", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Wastewater and sewage infrastructure system." }
+  { title: "Κύρια Είσοδος", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Main entrance overview and layout." },
+  { title: "Εμπρός Αυλή", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Front yard design and structure." },
+  { title: "Πίσω Αυλή", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rear yard planning and usage." },
+  { title: "Ισόγεια Κεραμοσκεπή", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rear patio functional space design." },
+  { title: "Πίσω Κάτω Σκάλα", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lower rear access and circulation." },
+  { title: "Κάτω Καθιστικό", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lower level living room arrangement." },
+  { title: "Εσωτερική Σκάλα", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Staircase flow and vertical connection." },
+  { title: "Επάνω Χώλ", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Upper hall circulation space." },
+  { title: "Επάνω Κύριο Υπνοδωμάτιο", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Master bedroom layout and comfort." },
+  { title: "Επάνω Καθιστικό", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Upper living area design concept." },
+  { title: "Επάνω Εμπρός Είσοδος", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Upper front access point design." },
+  { title: "Διαχείριση Απορριμάτων", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Wastewater and sewage infrastructure system." }
 ];
 
 /* LIST VIEW */
