5f66349038be4a9a7bea5cea7ca0bc4baa178083
georgep1121 <g.papastavrou@outlook.com>
2026-06-14 17:25:18 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,11 @@
-const CACHE_NAME = "home-manual-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
